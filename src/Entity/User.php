34d99582df6afe179ba27880c1b36357273cab12
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-07-21 13:16:32 +0200

    Remove apiresource and groups attributes

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,22 +14,14 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ApiResource(
-    operations: [
-        new Get(normalizationContext: ['groups' => ['user:read']]),
-        new GetCollection(normalizationContext: ['groups' => ['user:read']]),
-    ]
-)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['user:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
-    #[Groups(['article:list', 'user:read'])]
     private ?string $username = null;
 
     #[ORM\Column]
@@ -39,7 +31,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: Article::class)]
-    #[Groups(['user:read'])]
     private Collection $articles;
 
     public function __construct()
