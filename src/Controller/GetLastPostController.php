36e4387c0fada5a2a49cd17b02d2479c7704c2a8
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-07-25 14:32:48 +0200

    Remove AsController attribute

--- src/Controller/GetLastPostController.php
+++ src/Controller/GetLastPostController.php
@@ -5,11 +5,9 @@ namespace App\Controller;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-#[AsController]
 class GetLastPostController extends AbstractController
 {
     #[Route('/api/articles/last', name: 'article_get_last_post', methods: ['GET'])]
