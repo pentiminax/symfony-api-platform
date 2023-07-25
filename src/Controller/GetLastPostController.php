c43ea274560903ab73e9b5d7a32205543494b2aa
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-07-25 14:00:10 +0200

    Add GetLastPostController

---
+++ src/Controller/GetLastPostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+#[AsController]
+class GetLastPostController extends AbstractController
+{
+    #[Route('/api/articles/last', name: 'article_get_last_post', methods: ['GET'])]
+    public function getLastArticle(ArticleRepository $articleRepo, NormalizerInterface $normalizer): Response
+    {
+        return $this->json($normalizer->normalize($articleRepo->findLastArticle(), context: [
+            'groups' => ['article:read']
+        ]));
+    }
+}
